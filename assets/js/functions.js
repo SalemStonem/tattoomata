3a5699411a33ad96aadc016794624262832a598e
Ricardo <ricardo_caceres1995@hotmail.com>
2019-06-24 14:28:32 -0500

    Tattoo Mata 0.0.0

---
+++ assets/js/functions.js
@@ -0,0 +1,16 @@
+// var prevScrollpos = window.pageYOffset;
+// const el = document.getElementById('menu-container');
+// window.onscroll = function() {
+//   var currentScrollPos = window.pageYOffset;
+//   // document.getElementById("logo-container").style.top = "74px";
+//   el.classList.add('fixed-top');
+//   if (prevScrollpos > currentScrollPos) {
+//     // document.getElementById("logo-container").style.top = "0";
+//     el.classList.toggle('fixed-top');
+//   } else {
+//     // document.getElementById("logo-container").style.top = "-74px";
+//     // el.classList.remove('fixed-top');
+//   }
+//   prevScrollpos = currentScrollPos;
+//
+// }
