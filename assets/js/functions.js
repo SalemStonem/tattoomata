928839d2c861c1e1e41c5eb5391255914cc47cf6
Ricardo <ricardo_caceres1995@hotmail.com>
2019-07-23 01:40:35 -0500

    Tattoo Mata 2.0.3

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -16,91 +16,153 @@ $(document).ready(function() {
   //   prevScrollpos = currentScrollPos;
   //
   // }
-  const productos = [{
-      tipo: "maquinas",
-      palabras: ["maquina", "maquinas", "original", "maquinas originales"]
-    },
-    {
-      tipo: "tintas",
-      palabras: ["set", "tintas", "set de tintas", "tintas set", "set", "tinta"]
-    }, {
-      tipo: "agujas",
-      ["zita", "black rose"]
-    },
-    {
-      tipo: "cartuchos",
-      palabras: ["cartuchos", "big", "wasp", "big wasp", "cartuchos membrana", "Spark", "spark", "cartridge", "cartridge needles", "stigma", "menbrana"]
-    },
-    {
-      tipo: "pen",
-      palabras: ["pen", "pen rotativas", "pluma", "maquina pen"]
-    },
-    {
-      tipo: "rotativas",
-      palabras: ["rotativas", "rotativa", "maquina rotativa", "maquina de cartuchos"]
-    },
-    {
-      tipo: "bobinas",
-      palabras: ["bobina", "bobinas", "maquina de bobinas"]
-    },
-    {
-      tipo: "starbrite",
-      palabras: ["star", "starbrite", "tinta starbrite"]
-    },
-    {
-      tipo: "intenze",
-      palabras: ["intenze", "intense", "intence", "tinta intenze"]
-    },
-    {
-      tipo: "eternal",
-      palabras: ["eternal", "eternal ink", "ink eternal", "tinta eternal", "eternal set" , "eternal sets" , "halo set", "levgen", "m series", "liz", "liz colors", "liz cook", "rember", "rember set", "mike", "mike chambers", "chambers", "myke chambers"]
-    },
-    {
-      tipo: "dynamic",
-      palabras: ["dynamic", "dinamic", "dynamic colors", "dinamyc"]
-    },
-    {
-      tipo: "accesorios",
-      palabras: ["acesorios", "accesorios", "kit", "kits", "principiante", "kit para principiante"]
-    },
-    {
-      tipo: "kuro sumi",
-      palabras: ["kuro sumi", "kuro", "sumi"]
-    },
-    {
-      tipo: "fuentes",
-      palabras: ["fuente", "fuentes", "poder", "fuentes de poder"]
-    },
-    {
-      tipo: "muebles",
-      palabras: ["mueble", "muebles", "posabrazo", "posabraso", "camilla", "banco", "bancos", "silla", "sillas"]
-    },
-  ];
-
-
-
-
   const buscador = document.querySelector('#buscador');
 
   $('#send').click(function() {
-    console.log('puto');
     const texto = buscador.value.toLowerCase();
-    const filtrar = () => {
-      if (texto == "tintas") {
-        location.href = "http://www.fento.com.mx";
-        console.log('hola');
-      }
-      if (texto == "maquinas") {
-        location.href = "http://www.cristalab.com/foros";
-      }
-    };
+    switch (texto) {
+      case 'tintas':
+      case 'set':
+      case 'sets':
+      case 'set de tintas':
+        location.href = "tintas.html";
+        break;
+      case 'cartuchos':
+      case 'big':
+      case 'wasp':
+      case 'big wasp':
+      case 'membrana':
+      case 'cartuchos membrana':
+      case 'caruchos de membrana':
+      case 'spark':
+      case 'cartridge':
+      case 'cartridge needles':
+      case 'stigma':
+      case 'cartuchos stigma':
+        location.href = "cartuchos.html";
+        break;
+      case 'bobina':
+      case 'bobinas':
+      case 'maquinas de bobinas':
+      case 'maquina de bobina':
+      case 'maquina artesanal ':
+        location.href = "bobinas.html";
+        break;
+      case 'pen':
+      case 'maquina pen':
+      case 'maquinas pen':
+      case 'pens':
+      case 'maquina tipo pluma':
+      case 'rotativa tipo pen':
+        location.href = "pen.html";
+        break;
+      case 'rotativa':
+      case 'rotativas':
+      case 'maquinas rotativas':
+      case 'maquina rotativa':
+        location.href = "rotativas.html";
+        break;
+      case 'agujas y cartuchos':
+        location.href = "productos.html";
+        break;
+      case 'agujas y tubos':
+      case 'tubos':
+      case 'agujas':
+      case 'tubos y agujas':
+      case 'grips':
+      case 'grip':
+        location.href = "agujas-y-tubos.html";
+        break;
+      case 'accesorios':
+      case 'acesorios':
+      case 'pieles':
+      case 'piel sintetica':
+      case 'vendas':
+      case 'kits':
+      case 'venda':
+      case 'kit para tatuar':
+      case 'kit para tattoo':
+      case 'kits':
+      case 'kits para tatuaje':
+      case 'cubre maquina':
+      case 'cubre maquinas':
+      case 'clipcord':
+      case 'cables':
+      case 'cubre grip':
+        location.href = "accesorios.html";
+        break;
+      case 'muebles':
+      case 'posabrazo':
+      case 'posabraso':
+      case 'bancos':
+      case 'banco':
+      case 'camilla':
+        location.href = "muebles.html";
+        break;
+      case 'fuentes':
+      case 'fuente de poder':
+      case 'alimentacion':
+      case 'fuente para maquina':
+      case 'fuente para maquina rotativa':
+      case 'fuente para maquina de bobina':
+      case 'fuente':
+        location.href = "fuentes.html";
+        break;
+      case 'eternal':
+      case 'eternal ink':
+      case 'tinta eternal':
+      case 'tintas eternal':
+      case 'sets eternal':
+      case 'eternal set':
+      case 'eternal sets':
+      case 'halo set':
+      case 'levgen':
+      case 'liz cook':
+      case 'm series':
+      case 'myke chambers':
+      case 'chambers set':
+      case 'rember set':
+        location.href = "eternal.html";
+        break;
+      case 'intenze':
+      case 'intense':
+      case 'alex del pase':
+      case 'bob tyrrel':
+      case 'bowery':
+      case 'dragon color':
+      case 'set intenze':
+      case 'intenze set':
+      case 'tintas intenze':
+        location.href = "intenze.html";
+        break;
+      case 'dynamic':
+      case 'dinamic':
+      case 'dinamyc':
+      case 'dynamic colors':
+      case 'black':
+      case 'triple black':
+      case 'white':
+        location.href = "dynamic.html";
+        break;
+      case 'kuro sumi':
+      case 'kuro':
+      case 'sumi':
+      case 'tinta japonesa':
+      case 'tinta kuro sumi':
+        location.href = "kuro-sumi.html";
+        break;
+      case 'starbrite':
+      case 'tintas starbrite':
+      case 'tinta starbrite':
+      case 'star':
+      case 'brite':
+      case 'starbrite colors':
+        location.href = "starbrite.html";
+        break;
+      default:
+        document.getElementById("buscador").placeholder = "No encontrado";
+    }
   });
-
-  //
-
-
-  // const boton = document.querySelector('#send');
-  // const resultado = document.querySelector('#resultado')
 
 
 
